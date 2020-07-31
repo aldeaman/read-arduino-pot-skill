2ecc705e228b12a3228badbd74c9dee048448c3a
aldeaman <evan@evangould.com>
2020-07-31 02:24:59 +0100

    modified:   __init__.py 	modified:   locale/en-us/pot.arduino.read.dialog

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import serial
 
 class ReadArduinoPot(MycroftSkill):
     def __init__(self):
@@ -7,7 +7,11 @@ class ReadArduinoPot(MycroftSkill):
 
     @intent_file_handler('pot.arduino.read.intent')
     def handle_pot_arduino_read(self, message):
-        self.speak_dialog('pot.arduino.read')
+        ser = serial.Serial('/dev/ttyUSB0', 115200)
+        self.sensor_reading=(ser.readline()).decode('utf-8')
+        datadic = {"reading": self.sensor_reading}
+        self.log.info("THE SENSOR READING IS " + str(self.sensor_reading))
+        self.speak_dialog('pot.arduino.read', datadic)
 
 
 def create_skill():
